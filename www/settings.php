c30ff47dc11481edcf548d44b6c2bb59b52ffac4
Tim <tim.renner@pincsf.org>
2018-06-02 08:56:59 -0700

    moved misplaced config

--- www/settings.php
+++ www/settings.php
@@ -2,4 +2,4 @@
 
 $settings['hash_salt'] = '_Wolv5ML7pEXZVC4wpz8BZybWE7jAoMltkwjho9p18o';
 $config_directories['active'] = '../config/active';
-$config_directories['staging'] = '../files/config/staging';
+$config_directories['staging'] = '../config/staging';
