d53e66302be3abc11d7b3d791cf6c300f0e057c2
Tim <tim.renner@pincsf.org>
2018-06-02 08:43:56 -0700

    update site name

---
+++ www/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$settings['hash_salt'] = '_Wolv5ML7pEXZVC4wpz8BZybWE7jAoMltkwjho9p18o';
+$config_directories['active'] = '../config/active';
+$config_directories['staging'] = '../files/config/staging';
