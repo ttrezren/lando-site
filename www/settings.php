b74fd1c8eb4cf79f5fbaf2b9c979d219c228b48b
PINC <pincsf@server-imac.gateway.sonic.net>
2018-07-05 17:34:38 -0700

    theme changes

--- www/settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$settings['hash_salt'] = '_Wolv5ML7pEXZVC4wpz8BZybWE7jAoMltkwjho9p18o';
-$config_directories['active'] = '../config/active';
-$config_directories['staging'] = '../config/staging';
